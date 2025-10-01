ea4b7d8cababe1df6bf68baa2dd01bc0163f8fb7
EricCaverly <ecaverly1@nait.ca>
2025-09-30 18:29:37 -0600

    imported ssd1306 lib

--- main/main.c
+++ main/main.c
@@ -2,9 +2,12 @@
 #include "freertos/idf_additions.h"
 #include "portmacro.h"
 #include "tester.h"
+#include "ssd1306.h"
 
 void app_main(void) {
     int i = 0;
+    SSD1306_t dev;
+
     init_gpio();
 
     while (1) {
